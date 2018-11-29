1784d713fde0d09d154920d1552e10bf70b3d04b
Ben Burwell <ben@benburwell.com>
2018-11-29 11:46:41 -0500

    First pass at Travis CI

--- Makefile
+++ Makefile
@@ -1,7 +1,15 @@
-BINARY=runeflow
-CLI_SOURCES=$(shell find ./cli -name '*.go')
-BUILD_DIR=build
+BINARY = runeflow
+CLI_SOURCES = $(shell find ./cli -name '*.go')
+BUILD_DIR = build
+ARCH = amd64
+VERSION ?= dev
 
-$(BUILD_DIR)/$(BINARY): $(CLI_SOURCES)
-	GOOS=linux go build -o "$@" ./cli
+LDFLAGS = "-X main.version=$(VERSION) -X main.buildDate=$(shell date -u +%Y-%m-%dT%H:%M:%S) -X main.gitRevision=$(shell git rev-parse HEAD)"
 
+$(BUILD_DIR)/$(BINARY)_$(VERSION)_$(ARCH): $(CLI_SOURCES)
+	git rev-parse HEAD
+	GOOS=linux GOARCH="$(ARCH)" go build -ldflags $(LDFLAGS) -o "$@" ./cli
+
+.PHONY: clean
+clean:
+	rm -rf "$(BUILD_DIR)"
--- cli/main.go
+++ cli/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/runeflow/runeflow/api"
 	"github.com/runeflow/runeflow/client"
 	"github.com/runeflow/runeflow/config"
 )
+
+var version string
+var buildDate string
+var gitRevision string
 
 func main() {
 	if len(os.Args) < 2 {
@@ -26,12 +31,21 @@ func main() {
 	case "auth":
 		auth(a)
 		os.Exit(0)
-	case "-h":
-	case "--help":
-	case "help":
+	case "-h", "--help", "help":
 		usage()
+		os.Exit(0)
+	case "-v", "--version":
+		showVersion()
 		os.Exit(0)
 	}
 	usage()
 	os.Exit(1)
+}
+
+func showVersion() {
+	fmt.Println("Runeflow CLI")
+	fmt.Printf("Version: %s\n", version)
+	fmt.Printf("Compiled on: %s\n", buildDate)
+	fmt.Printf("Git revision: %s\n", gitRevision)
+	fmt.Printf("Source URL: https://github.com/runeflow/runeflow/tree/%s\n", gitRevision)
 }
