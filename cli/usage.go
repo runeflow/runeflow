70817c08806f98038a7c1a358bb79144e42ec641
Ben Burwell <ben@benburwell.com>
2018-11-30 11:26:19 -0500

    Improve usage details

--- cli/usage.go
+++ cli/usage.go
@@ -5,6 +5,9 @@ import "fmt"
 func usage() {
 	fmt.Println(`runeflow usage
 
-do some stuff
+runeflow register - create a new Runeflow account
+runeflow auth     - add this server to your Runeflow account
+
+For more details, check the manual page for runeflow(1).
 `)
 }
